9701d61b666a92714822b70a1b455538637b8cd8
fuzzbury <40711383+fuzzbury@users.noreply.github.com>
2020-05-30 07:14:26 +0100

    added a third quiz

--- film_round.py
+++ film_round.py
@@ -9,7 +9,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--directory", help="full directory path of the clips you want to use")
-parser.add_argument("--quiz", help="default quiz to use", default=1)
+parser.add_argument("--quiz", help="default quiz to use", default=3)
 args = parser.parse_args()
 
 FPS = 30  # frames per second, the general speed of the program
